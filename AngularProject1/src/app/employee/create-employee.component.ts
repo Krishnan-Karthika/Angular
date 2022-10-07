bd093737f29abbf3db9cb6915d1051369122296b
Karthika Krishnan <kkrishnan@orthofx.com>
2022-10-07 15:39:20 +0530

    move validation message to component class

--- AngularProject1/src/app/employee/create-employee.component.ts
+++ AngularProject1/src/app/employee/create-employee.component.ts
@@ -8,32 +8,71 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 })
 export class CreateEmployeeComponent implements OnInit {
   employeeForm!: FormGroup;
+
+  formErrors = {
+    'fullName': '',
+    'email': '',
+    'skillName': '',
+    'experienceInYears': '',
+    'proficiency': ''
+  };
+
+  validationMessages = {
+    'fullName': {
+      'required': 'Full Name is required.',
+      'minlength': 'Full Name must be greater than 2 characters.',
+      'maxlength': 'Full Name must be less than 10 characters.'
+    },
+    'email': {
+      'required': 'Email is required.'
+    },
+    'skillName': {
+      'required': 'Skill Name is required.',
+    },
+    'experienceInYears': {
+      'required': 'Experience is required.',
+    },
+    'proficiency': {
+      'required': 'Proficiency is required.',
+    },
+  };
+
   constructor(private fb: FormBuilder) { }
 
   ngOnInit(){
     this.employeeForm = this.fb.group({
       fullName: ['',[Validators.required,Validators.minLength(2),Validators.maxLength(10)]],
-      email: [''],
+      email: ['',Validators.required],
       skills: this.fb.group({
-        skillName: [''],
-        experienceInYears: [''],
-        proficiency: ['beginner']
+        skillName: ['',Validators.required],
+        experienceInYears: ['',Validators.required],
+        proficiency: ['',Validators.required]
       }),
     });
   }
 
-  logKeyValuePairs(group: FormGroup): void {
+  logValidationErrors(group: FormGroup): void {
     Object.keys(group.controls).forEach((key: string) => {
       const abstractControl = group.get(key);
       if (abstractControl instanceof FormGroup) {
-        this.logKeyValuePairs(abstractControl);
+        this.logValidationErrors(abstractControl);
       } else {
-        abstractControl?.markAsDirty();      }
+        (this.formErrors as any)[key] = '';
+        if (abstractControl && !abstractControl.valid) {
+          const messages = (this.validationMessages as any)[key];
+          for (const errorKey in abstractControl.errors) {
+            if (errorKey) {
+              (this.formErrors as any)[key] += messages[errorKey] + ' ';
+            }
+          }
+        }
+      }
     });
   }
 
   onLoadDataClick(): void {
-    this.logKeyValuePairs(this.employeeForm);
+    this.logValidationErrors(this.employeeForm);
+    console.log(this.formErrors);
   }
 
   onSubmit(): void{
