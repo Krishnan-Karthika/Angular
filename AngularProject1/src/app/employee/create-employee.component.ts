1f082118bb1ae313841682ff5c9421e242987524
Karthika Krishnan <kkrishnan@orthofx.com>
2022-10-11 08:35:02 +0530

    include cross field validation

--- AngularProject1/src/app/employee/create-employee.component.ts
+++ AngularProject1/src/app/employee/create-employee.component.ts
@@ -1,67 +1,90 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  AbstractControl,
+} from '@angular/forms';
 import { CustomValidators } from '../shared/custom.validators';
 
 @Component({
   selector: 'app-create-employee',
   templateUrl: './create-employee.component.html',
-  styleUrls: ['./create-employee.component.css']
+  styleUrls: ['./create-employee.component.css'],
 })
 export class CreateEmployeeComponent implements OnInit {
   employeeForm!: FormGroup;
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder) {}
 
   formErrors = {
-    'fullName': '',
-    'email': '',
-    'phone': '',
-    'skillName': '',
-    'experienceInYears': '',
-    'proficiency': ''
+    fullName: '',
+    email: '',
+    confirmEmail: '',
+    emailGroup: '',
+    phone: '',
+    skillName: '',
+    experienceInYears: '',
+    proficiency: '',
   };
 
   validationMessages = {
-    'fullName': {
-      'required': 'Full Name is required.',
-      'minlength': 'Full Name must be greater than 2 characters.',
-      'maxlength': 'Full Name must be less than 10 characters.'
+    fullName: {
+      required: 'Full Name is required.',
+      minlength: 'Full Name must be greater than 2 characters.',
+      maxlength: 'Full Name must be less than 10 characters.',
     },
-    'email': {
-      'required': 'Email is required.',
-      'emailDomain': 'Email domain should be dell.com'
+    email: {
+      required: 'Email is required.',
+      emailDomain: 'Email domain should be dell.com',
     },
-    'phone': {
-      'required': 'Phone is required.'
+    confirmEmail: {
+      required: 'Confirm Email is required.',
     },
-    'skillName': {
-      'required': 'Skill Name is required.',
+    emailGroup: {
+      emailMismatch: 'Email and Confirm Email do not match.'
     },
-    'experienceInYears': {
-      'required': 'Experience is required.',
+    phone: {
+      required: 'Phone is required.',
     },
-    'proficiency': {
-      'required': 'Proficiency is required.',
+    skillName: {
+      required: 'Skill Name is required.',
+    },
+    experienceInYears: {
+      required: 'Experience is required.',
+    },
+    proficiency: {
+      required: 'Proficiency is required.',
     },
   };
 
-
-
-  ngOnInit(){
+  ngOnInit() {
     this.employeeForm = this.fb.group({
-      fullName: ['',[Validators.required,Validators.minLength(2),Validators.maxLength(10)]],
+      fullName: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(2),
+          Validators.maxLength(10),
+        ],
+      ],
       contactPreference: ['email'],
-      email: ['',[Validators.required,CustomValidators.emailDomain('dell.com')]],
+      emailGroup: this.fb.group({
+        email: ['',[Validators.required, CustomValidators.emailDomain('dell.com')]],
+        confirmEmail: ['', Validators.required],
+      },{ validator: matchEmails }),
       phone: [''],
       skills: this.fb.group({
-        skillName: ['',Validators.required],
-        experienceInYears: ['',Validators.required],
-        proficiency: ['',Validators.required]
+        skillName: ['', Validators.required],
+        experienceInYears: ['', Validators.required],
+        proficiency: ['', Validators.required],
       }),
     });
 
-    (this.employeeForm as any).get('contactPreference').valueChanges.subscribe((data: string) => {
-      this.onContactPrefernceChange(data);
-  });
+    (this.employeeForm as any)
+      .get('contactPreference')
+      .valueChanges.subscribe((data: string) => {
+        this.onContactPrefernceChange(data);
+      });
 
     this.employeeForm.valueChanges.subscribe((data) => {
       this.logValidationErrors(this.employeeForm);
@@ -81,11 +104,13 @@ export class CreateEmployeeComponent implements OnInit {
   logValidationErrors(group: FormGroup = this.employeeForm): void {
     Object.keys(group.controls).forEach((key: string) => {
       const abstractControl = group.get(key);
-      if (abstractControl instanceof FormGroup) {
-        this.logValidationErrors(abstractControl);
-      } else {
-        (this.formErrors as any)[key] = '';
-        if (abstractControl && !abstractControl.valid && (abstractControl.touched || abstractControl.dirty)) {
+
+      (this.formErrors as any)[key] = '';
+        if (
+          abstractControl &&
+          !abstractControl.valid &&
+          (abstractControl.touched || abstractControl.dirty)
+        ) {
           const messages = (this.validationMessages as any)[key];
           for (const errorKey in abstractControl.errors) {
             if (errorKey) {
@@ -93,6 +118,9 @@ export class CreateEmployeeComponent implements OnInit {
             }
           }
         }
+
+      if (abstractControl instanceof FormGroup) {
+        this.logValidationErrors(abstractControl);
       }
     });
   }
@@ -102,15 +130,24 @@ export class CreateEmployeeComponent implements OnInit {
     // console.log(this.formErrors);
   }
 
-  onSubmit(): void{
+  onSubmit(): void {
     // console.log(this.employeeForm);
     console.log(this.employeeForm.value);
     console.log(this.employeeForm.touched);
 
     console.log(this.employeeForm.controls['fullName'].value);
     console.log(this.employeeForm.get('fullName')?.value);
-
   }
 }
 
+function matchEmails(group: AbstractControl): { [key: string]: any } | null {
+  const emailControl = group.get('email');
+  const confirmEmailControl = group.get('confirmEmail');
 
+  if (
+    emailControl?.value === confirmEmailControl?.value || confirmEmailControl?.pristine) {
+    return null;
+  } else {
+    return { emailMismatch: true };
+  }
+}
