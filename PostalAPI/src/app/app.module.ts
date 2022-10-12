d598072e689d057643bf4e921454749d435fe70c
Karthika Krishnan <kkrishnan@orthofx.com>
2022-10-12 08:24:51 +0530

    page format created

--- PostalAPI/src/app/app.module.ts
+++ PostalAPI/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchPageComponent } from './searchpage/search-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchPageComponent
   ],
   imports: [
     BrowserModule,
